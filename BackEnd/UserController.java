26c4c2ea7d8329cc5ffba68a869807ee3f7e00c7
sanju250406 <69379304+sanju250406@users.noreply.github.com>
2024-09-21 15:33:51 +0530

    UserIdentification by Emails
    Users will be identified by their emails not their IDs

--- BackEnd/UserController.java
+++ BackEnd/UserController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/users")
@@ -15,16 +18,24 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestParam String email, @RequestParam String password) {
-        User newUser = userService.createUser(email, password);
+    public ResponseEntity<User> createUser(
+            @RequestParam String email,
+            @RequestParam String password,
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dob) {
+        
+        User newUser = userService.createUser(email, password, firstName, lastName, dob);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
-        User user = userService.getUserById(id);
+    @GetMapping("/email/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        User user = userService.getUserByEmail(email);
         return ResponseEntity.ok(user);
     }
+
+    // Removed the getUserById method since users are now identified by email
 
     // Additional methods for updating and deleting users can be added here
 }
